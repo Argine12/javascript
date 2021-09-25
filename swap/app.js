ca28de3c7538774bd0ec4d27d6d6d34eea4ecfdc
Argine12 <85347056+Argine12@users.noreply.github.com>
2021-09-25 13:14:05 +0400

    Add files via upload

--- swap/app.js
+++ swap/app.js
@@ -1,1 +1,17 @@
+let a = 4;
+let b = 120;
 
+[a,b]=[b,a]
+
+console.log(a,b);
+
+
+//or
+
+let arr = [2, 5];
+
+    let temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp
+
+    console.log(arr);
