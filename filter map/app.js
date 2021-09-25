4d47e386de319831fb5221a87f6098f6df0f8451
Argine12 <85347056+Argine12@users.noreply.github.com>
2021-09-25 13:18:15 +0400

    Add files via upload

--- filter map/app.js
+++ filter map/app.js
@@ -1,1 +1,17 @@
+//Filter
+const arr = ['Bolivia', 'Armenia', 'China', 'Canada', 'Latvia', 'Moldova', 'Korea'];
 
+const newArr = arr.filter(Arr => Arr.length < 7);
+
+console.log(newArr);
+
+
+//map
+
+const arr1 = [2, 5, 4, 3, 7, 8, 16];
+
+const arrMap = arr1.map((elem, indes) => {
+    return elem = elem % 2 == 1 ;
+});
+
+console.log(arrMap);
