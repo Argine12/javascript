a8676f06d2ce25c65854c890c9a6b4c24b7136b8
Argine12 <85347056+Argine12@users.noreply.github.com>
2021-09-25 13:23:08 +0400

    Add files via upload

--- synchronous/app.js
+++ synchronous/app.js
@@ -1,1 +1,29 @@
+//Asynchronus
 
+console.log("Argine");
+setTimeout(() => {
+    console.log("Melkumyn");
+}, 10000);
+
+console.log(18);
+
+
+
+
+//Sinchronus
+
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    showData() {
+        console.log(this.firstName, this.lastName, this.age);
+    }
+}
+
+const p = new Person( 'Argine', 'Melkumyan', 18)
+
+p.showData();
