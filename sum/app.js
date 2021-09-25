4d458d254af14f1ba2a3bc643eeb0d4de1120c46
Argine12 <85347056+Argine12@users.noreply.github.com>
2021-09-25 13:13:03 +0400

    Add files via upload

--- sum/app.js
+++ sum/app.js
@@ -1,1 +1,10 @@
+var arr0=[1,2,3];
+var array1=[4,5,6];
+var arr2=[7,8,9];
+
+console.log(arr0[0]+arr1[1]+arr2[2]);
+console.log(arr0[2]+arr1[1]+arr2[0]);
+
+
+console.log(arr0[0]+arr1[1]+arr2[2]+arr0[2]+arr1[1]+arr2[0]);
 
